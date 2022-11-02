5bc7a235532044394c07b50870a448a1065f7f3b
Sathyakumar <93641901+Febiecode@users.noreply.github.com>
2022-11-02 11:54:04 +0530

    Add files via upload

---
+++ near/api/getter.ts
@@ -0,0 +1,25 @@
+import type {NextApiRequest, NextApiResponse} from 'next';
+import {configFromNetwork} from '@figment-near/lib';
+import {connect} from 'near-api-js';
+
+export default async function (
+  req: NextApiRequest,
+  res: NextApiResponse<string>,
+) {
+  const {network, accountId} = req.body;
+  try {
+    const config = configFromNetwork(network);
+    const near = await connect(config);
+    const account = await near.account(accountId);
+    // Using ViewFunction try to call the contract
+    const response = await account.viewFunction(
+      accountId,
+      "get_greeting",
+      {account_id: accountId}
+  );
+    return res.status(200).json(response);
+  } catch (error) {
+    let errorMessage = error instanceof Error ? error.message : 'Unknown Error';
+    return res.status(500).json(errorMessage);
+  }
+}
---
+++ near/api/setter.ts
@@ -0,0 +1,30 @@
+import type {NextApiRequest, NextApiResponse} from 'next';
+import {configFromNetwork} from '@figment-near/lib';
+import {connect, KeyPair} from 'near-api-js';
+
+export default async function (
+  req: NextApiRequest,
+  res: NextApiResponse<string>,
+) {
+  const {network, accountId, secret, newMessage} = req.body;
+  try {
+    const config = configFromNetwork(network);
+    const keypair = KeyPair.fromString(secret);
+    config.keyStore?.setKey('testnet', accountId, keypair);
+
+    const near = await connect(config);
+    const account = await near.account(accountId);
+    // Look at functionCall and pass the expected args
+    // ... fill here
+    const optionsCall = {
+      contractId: accountId,
+      methodName: 'set_greeting',
+      args: { message: newMessage }
+  }
+  const response = await account.functionCall(optionsCall);
+    return res.status(200).json(response.transaction.hash);
+  } catch (error) {
+    let errorMessage = error instanceof Error ? error.message : 'Unknown Error';
+    return res.status(500).json(errorMessage);
+  }
+}
